92ad82c1b4a37c9025a007314695c5fa38bfd4a1
thanujsai <sboda@modeln.com>
2024-03-29 15:47:51 +0530

    put and delete requests

--- script.js
+++ script.js
@@ -19,15 +19,16 @@ const getTodos = () => {
 
 const addToDotoDOM = (element) => {
     const div = document.createElement('div');
-                div.appendChild(document.createTextNode(element.title));
-                div.setAttribute('data-id',element.id);
+    div.classList.add('todo');
+    div.appendChild(document.createTextNode(element.title));
+    div.setAttribute('data-id',element.id);
 
-                if(element.completed){
-                    console.log(`completed element`)
-                    console.log(element)
-                    div.classList.add('done');
-                }
-                document.getElementById('todo-list').appendChild(div)
+    if(element.completed){
+        console.log(`completed element`)
+        console.log(element)
+        div.classList.add('done');
+    }
+    document.getElementById('todo-list').appendChild(div)
 };
 
 const createTodo = (e) => {
@@ -49,12 +50,48 @@ const createTodo = (e) => {
 }
 
 const toggleCompleted = (e) => {
-    e.target.classList.toggle('done');
+    console.log(`in toggle completed`)
+    if(e.target.classList.contains('done')){
+        console.log(`in if of toggle completed`)
+        e.target.classList.toggle('done');
+
+        updateTodo(e.target.dataset.id, e.target.classList.contains('done'));
+    }
+}
+
+const updateTodo = (id,completed) =>{
+    fetch(`${apiUrl}/${id}`,{
+        method: 'PUT',
+        body: JSON.stringify({completed}),
+        headers:{
+            'Content-Type':'application/json'
+        }
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))
+}
+
+const deleteTodo = (e) => {
+    console.log(e.target.classList)
+    if(e.target.classList.contains('todo')){
+        console.log(`delete`)
+        e.target.classList.toggle('done');//When this code is executed in response to an event (e.g., a click event), it will add the 'done' class to the target element if it doesn't already have it, or remove it if it does. 
+
+        const id = e.target.dataset.id;
+        fetch((`${apiUrl}/${id}`),{
+            method: 'DELETE'
+        })
+        .then(res => res.json())
+        .then(() => e.target.remove())
+        updateTodo(e.target.dataset.id, e.target.classList.contains('done'));
+    }
 }
 const init = () => {
     document.addEventListener('DOMContentLoaded',getTodos);
     document.querySelector('#todo-form').addEventListener('submit',createTodo);
     document.querySelector('#todo-list').addEventListener('click',toggleCompleted);
+    document.querySelector('#todo-list').addEventListener('dblclick',deleteTodo);//double click
+
 }
 
 init();
